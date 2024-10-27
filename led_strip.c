404418de8e552a13bc49581d1fe8a557c61d0620
Tim Berglund <tlberglund@gmail.com>
2024-10-27 06:24:52 -0700

    Working color march

--- led_strip.c
+++ led_strip.c
@@ -84,18 +84,18 @@ void hsl_to_rgb(HSL *hsl, RGB *rgb) {
 
     // Normalize saturation and lightness to the range 0.0 - 1.0
     float s_f = hsl->saturation / 1000.0;
-    printf("%1.7f,", s_f);
+    // printf("%1.7f,", s_f);
     float l_f = hsl->lightness / 1000.0;
-    printf("%1.7f,", l_f);
+    // printf("%1.7f,", l_f);
 
     // Calculate chroma
     float c = (1.0f - fabs(2.0f * l_f - 1.0f)) * s_f;
-    printf("%1.7f,", c);
+    // printf("%1.7f,", c);
 
     float x = c * (1.0f - fabs(fmod(hsl->hue / 60.0, 2) - 1.0f));
-    printf("%1.7f,", x);
+    // printf("%1.7f,", x);
     float m = l_f - c / 2.0;
-    printf("%1.7f,", m);
+    // printf("%1.7f,", m);
 
     float r1 = 0, g1 = 0, b1 = 0;
 
@@ -114,7 +114,7 @@ void hsl_to_rgb(HSL *hsl, RGB *rgb) {
         r1 = c; g1 = 0; b1 = x;
     }
 
-    printf("%1.7f,%1.7f,%1.7f,",r1,g1,b1);
+    // printf("%1.7f,%1.7f,%1.7f,",r1,g1,b1);
 
     // Convert to 0-255 range and store the values in the RGB variables
     rgb->red = clamp((r1 + m) * 255);
@@ -125,19 +125,19 @@ void hsl_to_rgb(HSL *hsl, RGB *rgb) {
 }
 
 uint32_t iteration;
-uint16_t next_hue = 0, last_hue = 0;
-float fitered_h = 180.0, filtered_l = 500.0, filtered_s = 500.0;
-float filtered_r = 1.0, filtered_g = 1.0, filtered_b = 1.0;
+float filtered_h = 180.0, filtered_l = 500.0, filtered_s = 500.0;
 
-#define FILTER_ALPHA 1.0
+#define FILTER_ALPHA 0.1
 float filter_float(float previous, float new) {
     return ((FILTER_ALPHA * new) + ((1.0 - FILTER_ALPHA) * previous));
 }
 
 bool led_strip_frame_callback(__unused struct repeating_timer *t) {
+    RGB rgb;
+    HSL hsl;
 
+/*
     uint16_t adc_hue, adc_lightness, adc_saturation;
-
     adc_select_input(0);
     adc_hue = adc_read();
     adc_select_input(1);
@@ -145,12 +145,11 @@ bool led_strip_frame_callback(__unused struct repeating_timer *t) {
     adc_select_input(2);
     adc_saturation = adc_read();
 
-    RGB rgb;
-    HSL hsl;
 
-    fitered_h = filter_float(fitered_h, adc_hue / 4096.0 * 360.0);
+    filtered_h = filter_float(filtered_h, adc_hue / 4096.0 * 360.0);
     filtered_l = filter_float(filtered_l, adc_lightness / 4096.0 * 1000.0);
     filtered_s = filter_float(filtered_s, adc_saturation / 4096.0 * 1000.0);
+    printf("%03.1f,%04.1f,%04.1f\n", filtered_h, filtered_s, filtered_l);
 
     hsl.hue = (uint16_t)fitered_h;
     hsl.lightness = (uint16_t)filtered_l;
@@ -160,57 +159,50 @@ bool led_strip_frame_callback(__unused struct repeating_timer *t) {
            adc_hue, adc_saturation, adc_lightness,
            hsl.hue, hsl.saturation, hsl.lightness);
 
-
-    // filtered_r = filter_float(filtered_r, adc_hue / 4096.0 * 256.0);
-    // filtered_g = filter_float(filtered_g, adc_lightness / 4096.0 * 256.0);
-    // filtered_b = filter_float(filtered_b, adc_saturation / 4096.0 * 256.0);
-    // rgb.red = (uint8_t)filtered_r;
-    // rgb.green = (uint8_t)filtered_g;
-    // rgb.blue = (uint8_t)filtered_b;
-
     hsl_to_rgb(&hsl, &rgb);
 
-    printf("%03d,%03d,%03d\n",rgb.red, rgb.green, rgb.blue);
 
+    printf("%03d,%03d,%03d\n",rgb.red, rgb.green, rgb.blue);
     for(int i = 0; i < LED_STRIP_LEN; i++) {
         strip[i].green = rgb.green;
         strip[i].red = rgb.red;
         strip[i].blue = rgb.blue;
     }
+*/
 
-#if 0
-    int16_t animation_step = (iteration % 60) - 5;
+    adc_select_input(0);
+    uint16_t adc_brightness = adc_read();
+    int8_t brightness = ((float)adc_brightness / (4096.0 / 16.0));
+    int16_t animation_step = (iteration % 80) - 15;
+
+    filtered_l = 500.0;
+    filtered_s = 850.0;
+    filtered_h = 142.5;
+
     for(int i = 0; i < LED_STRIP_LEN; i++) {
         uint16_t distance_from_step = abs(i - animation_step);
-        int16_t lightness = 500 - (distance_from_step * 100);
+        int16_t lightness = filtered_l - (distance_from_step * 50);
         if(lightness < 0) {
-            hsl.lightness = 50;
-            hsl.saturation = 50;
-            hsl.hue = last_hue;
+            hsl.lightness = 0;
+            hsl.saturation = filtered_s;
+            hsl.hue = filtered_h;
         }
         else {
             hsl.lightness = lightness;
-            hsl.saturation = lightness >> 2;
-            hsl.hue = next_hue;
+            hsl.saturation = filtered_s;
+            hsl.hue = filtered_h;
         }
 
         hsl_to_rgb(&hsl, &rgb);
         
+        strip[i].brightness = brightness;
         strip[i].green = rgb.green;
         strip[i].red = rgb.red;
         strip[i].blue = rgb.blue;
     }
 
-    if((iteration % 240) == 0) {
-        last_hue = next_hue;
-        next_hue = get_rand_32() % 360;
-        printf("NEW TARGET HUE=%d\n", next_hue);
-    }
-
     iteration++;
-#endif
 
-     // printf("iteration %d\n", iteration);
     pico_set_led(true);
     apa102_strip_update(strip, LED_STRIP_LEN);
     pico_set_led(false);
